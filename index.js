76afc8702d1f378b9315df50ae8c8205f09cd03a
Fabio Tetsuo <fabioctetsuo@gmail.com>
2024-08-22 21:49:06 -0300

    add responsive layout

---
+++ index.js
@@ -0,0 +1,27 @@
+const tasks = [
+  {
+    title: "Implementar tela de listagem de tarefas",
+    tag: "frontend",
+    created_at: new Date().toISOString(),
+    done: false,
+  },
+  {
+    title: "Criar endpoint para cadastro de tarefas",
+    tag: "backend",
+    created_at: new Date().toISOString(),
+    done: false,
+  },
+  {
+    title: "Implementar protótipo da listagem de tarefas",
+    tag: "ux",
+    created_at: new Date().toISOString(),
+    done: true,
+  },
+];
+
+// window.onload = () => {
+//   console.log(tasks);
+//   tasks.forEach((task) => {
+//     document.createElement('article')
+//   })
+// };
